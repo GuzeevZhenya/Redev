4ce3f7e71f23aeb109d8a00e48da63ef66a02a95
GuzeevZhenya <shyst.evgeny@mail.ru>
2020-07-07 13:37:26 +0300

    add

---
+++ Переменные/script.js
@@ -0,0 +1,40 @@
+// task1
+let num = 3;
+console.log(num);
+
+
+// task2 
+let a = 10;
+let b = 2;
+let sum = a + b;
+let minus = a - b;
+let multiplication = a * b;
+let division = a / b;
+
+console.log(sum);
+console.log(minus);
+console.log(multiplication);
+console.log(division);
+
+//task3
+let c = 15;
+let d = 2;
+
+let result = c + d;
+console.log(result);
+
+//task 4 
+
+let a4 = 10;
+let b4 = 2;
+let c4 = 5;
+
+console.log(a4 + b4 + c4);
+
+//task5 
+let a5 = 17;
+let b5 = 10;
+let c5 = a5 - b5;
+let d5 = 7;
+let result5 = c5 + d5;
+console.log(result5);
