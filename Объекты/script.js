e2d545a020f433c7e251eeb3a7e487ee9a689a84
GuzeevZhenya <shyst.evgeny@mail.ru>
2020-08-03 13:15:31 +0300

    add

--- Объекты/script.js
+++ Объекты/script.js
@@ -82,3 +82,118 @@ console.log(obj1[6]);
 // 4)Пусть теперь номер дня недели хранится в переменной day, например там лежит число 3. Выведите день недели, соответствующий значению переменной day.
 let dayNumb = 3;
 console.log(obj1[dayNumb]);
+
+
+
+var calculateSalary = function(price) {
+    var percent = 0.35;
+    if (price >= 100000) {
+        var percent = 0.45;
+    }
+    let clear = price - price * percent;
+    return clear;
+}
+
+calculateSalary(2222222);
+
+
+
+var calculateMiles = function(distance, isBusinessClass) {
+    var percent = 0.18;
+    if (isBusinessClass) {
+        percent += 0.04;
+    }
+    if (distance > 3500) {
+        percent += 0.15;
+    }
+    return distance * percent;
+};
+
+var calculateFlights = function(distance, isBusinessClass, milesTarget) {
+    var miles = calculateMiles(distance, isBusinessClass);
+    console.log('Мили за перелёт: ' + miles);
+    var flights = Math.ceil(milesTarget / miles);
+    console.log('Количество полётов: ' + flights);
+};
+
+calculateFlights(3118, true, 15000);
+
+
+
+
+
+function getPrice(time, isSpeed) {
+    var priceHour = 1500;
+
+    if (isSpeed) {
+        time /= 2;
+        priceHour *= 2.5;
+    }
+
+    if (time > 150) {
+        priceHour -= 250;
+    }
+
+    var price = priceHour * time;
+    return price;
+}
+
+//console.log(getPrice(200, false));
+
+
+
+
+function getProfitableProject(hour, profit) {
+    var urgent = getPrice(hour, true) - profit;
+    var normal = getPrice(hour, false);
+    var result = '';
+    if (urgent < normal) {
+        result = 'Выгодней срочный проект. Потратишь на него ' + urgent;
+    } else {
+        result = 'Выгодней обычный проект. Потратишь на него ' + normal;
+
+    }
+    return result;
+}
+
+//   //let result = urgent - normal;
+
+
+//   return hour + profit;
+// }
+
+// getProfitableProject(12,2000);
+
+let user1 = {
+    name: "Джон"
+};
+let admin = {
+    name: "Админ"
+};
+
+function sayHi() {
+    //  alert(this.name);
+}
+
+// используем одну и ту же функцию в двух объектах
+user1.f = sayHi;
+admin.f = sayHi;
+
+user1.f();
+admin.f();
+console.log(user1);
+console.log(admin);
+
+let calculator = {
+    a: 2,
+    b: 3,
+    sum() {
+        return this.a + this.b;
+    },
+    mul() {
+        return this.a * this.b;
+    }
+};
+
+console.log(calculator.sum());
+console.log(calculator.mul());
---
+++ Числа/script.js
@@ -0,0 +1,29 @@
+var num = 3;
+console.log(num);
+
+
+var a = 10;
+var b = 2;
+console.log(a + b);
+console.log(a - b);
+console.log(a / b);
+console.log(a * b);
+
+
+var c = 15;
+var d = 2;
+var result;
+result = c + d;
+console.log(result);
+
+var a = 10;
+var b = 2;
+var c = 5;
+console.log(a + b + c);
+
+var a = 17;
+var b = 10;
+var c = a - b;
+var d = 7;
+var result = c + d;
+console.log(result);
