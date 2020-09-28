d7fe856fc5ac61129960fbc6f9a5fdafa54e6f26
GuzeevZhenya <shyst.evgeny@mail.ru>
2020-09-28 23:08:54 +0300

    add

--- Задание/main.js
+++ Задание/main.js
@@ -1,61 +1,54 @@
-function family(name, age, gender) {
+function Family(name, age, gender) {
     this.name = name;
     this.age = age;
     this.gender = gender;
-<<<<<<< HEAD
     this.sayFullInfo = function () {
-        console.log(this.name + " " + this.age + " " + this.gender + " ");
-    };
-    this.createWork = function (work) {
-        this.work = work;
-=======
-    this.sayFullInfo = function() {
         return `Имя: ${name}, Возраст ${age}, Пол: ${gender}`;
     };
-    this.createWork = function(work) {
+    this.createWork = function (work) {
         console.log(work);
->>>>>>> c6fd7753129e6a0af3d01d98aa2c9bed6accaef7
     };
 }
 
-var zhenya = new family('zhenya', 24, 'male');
-<<<<<<< HEAD
-zhenya.sayFullInfo();
-zhenya.createWork('Программист');
-
-var vania = new family('vania', 32, 'male');
-vania.sayFullInfo();
-zhenya.createWork('Техник-технолог');
-
-var sania = new family('sania', 25, 'male');
-sania.sayFullInfo();
-zhenya.createWork('Программист');
-
-var sergei = new family('sergei', 27, 'male');
-sergei.sayFullInfo();
-zhenya.createWork('Экономист');
-
-var andrei = new family('andrei', 14, 'male');
-andrei.sayFullInfo();
-zhenya.createWork('Школьник');
-=======
+var zhenya = new Family('zhenya', 24, 'male');
 console.log(zhenya.sayFullInfo());
 zhenya.createWork('Программист');
 
-
-var vania = new family('vania', 32, 'male');
+var vania = new Family('vania', 32, 'male');
 console.log(vania.sayFullInfo());
 vania.createWork('Техник-технолог');
 
-var sania = new family('sania', 25, 'male');
+var sania = new Family('sania', 49, 'male');
 console.log(sania.sayFullInfo());
 sania.createWork('Программист');
 
-var sergei = new family('sergei', 27, 'male');
+var sergei = new Family('sergei', 27, 'male');
 console.log(sergei.sayFullInfo());
 sergei.createWork('Экономист');
 
-var andrei = new family('andrei', 14, 'male');
+var andrei = new Family('andrei', 14, 'male');
 console.log(andrei.sayFullInfo());
 andrei.createWork('Школьник');
->>>>>>> c6fd7753129e6a0af3d01d98aa2c9bed6accaef7
+
+let allPerson = [];
+allPerson.push(zhenya);
+allPerson.push(vania);
+allPerson.push(sania);
+allPerson.push(sergei);
+allPerson.push(andrei);
+
+console.log(allPerson);
+
+
+// function oldestPerson(allPerson) {
+//     allPerson.sort(function (a, b) {
+//         return a.age - b.age;
+//     });
+//     return allPerson[allPerson.length - 1];
+// }
+
+let oldestPerson = (allPerson) => allPerson
+    .sort((a, b) => a.age - b.age)
+    .pop();
+
+console.log(oldestPerson(allPerson));
